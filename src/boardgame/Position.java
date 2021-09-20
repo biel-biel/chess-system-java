ec77e100ed2c624d2d5379ada64f737cb630fd8c
Gabriel Forneck <gabrielforneck2004@gmail.com>
2021-09-20 15:29:31 -0300

    Chess layer and printing the board

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -9,15 +9,19 @@ public class Position {
 		this.row = row;
 		this.column = column;
 	}
+	
 	public int getRow() {
 		return row;
 	}
+	
 	public void setRow(int row) {
 		this.row = row;
 	}
+	
 	public int getColumn() {
 		return column;
 	}
+	
 	public void setColumn(int column) {
 		this.column = column;
 	}
